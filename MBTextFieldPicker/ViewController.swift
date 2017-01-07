78cba5a2089b4cc113ee4170977f9cbe678cbc41
Manish <manishej004@gmail.com>
2017-01-07 15:55:15 +0530

    Adding Picker view.

--- MBTextFieldPicker/ViewController.swift
+++ MBTextFieldPicker/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleTextField: MBTextFieldPicker!
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleTextField.defaultSelectedString = nil
+        titleTextField.dataSet = ["Mr.","Mrs.","Miss"]
+        
+        titleTextField.setRightButton("Done", style: .default) {
+            self.titleTextField.defaultSelectedString = self.titleTextField.selectedString
+            print(self.titleTextField.selectedString ?? "Selected value")
+            self.titleTextField.closePicker()
+        }
+        
+        titleTextField.setLeftButton("Cancel", style: .close) {
+            self.titleTextField.showDefaultString()
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
